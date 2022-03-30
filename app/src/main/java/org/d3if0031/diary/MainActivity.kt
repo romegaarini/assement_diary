1222ad3d913207e23ef05e1b797a329d27cd7a00
romegaarini <romega.purba123@gmail.com>
2022-03-30 20:28:34 +0700

    membuat layout yang konsisten di berbagai ukuran

--- app/src/main/java/org/d3if0031/diary/MainActivity.kt
+++ app/src/main/java/org/d3if0031/diary/MainActivity.kt
@@ -2,10 +2,18 @@ package org.d3if0031.diary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.d3if0031.diary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    private fun diary(){
+        Log.d("MainActivity","Tombol diklik")
     }
 }
